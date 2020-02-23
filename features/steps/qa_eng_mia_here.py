52d26872f09cb34d5fb43ad5ec92871ae6cd4b23
LupusLudensEst <gurovvic@gmail.com>
2020-02-22 19:32:55 -0500

    upgrade dt 22 Feb 2020

---
+++ features/steps/qa_eng_mia_here.py
@@ -0,0 +1,17 @@
+from behave import *
+
+@given("Loginpage")
+def open_homepage(context):
+    context.app.main_page.open_page()
+
+@then("Click on careers button")
+def click_crs_btn(context):
+    context.app.main_page.click_crs_btn()
+
+@then("Click on QA Automation Engineer in Miami button inside the iframe")
+def click_qa_eng_mia(context):
+    context.app.main_page.click_qa_eng_mia()
+
+@then("Verify page has a text As {text_here}")
+def qa_txt_hr(context, text_here):
+    context.app.main_page.qa_txt_hr(text_here)
---
+++ pages/main_page.py
@@ -0,0 +1,30 @@
+from time import sleep
+from selenium.webdriver.common.by import By
+from pages.base_page import Page
+
+class MainPage(Page):
+
+    # locators
+    CRS_BTN = (By.XPATH, "//li[@class='menu-item menu-item-type-post_type menu-item-object-page menu-item-3954']")
+    QA_ENG_MIA = (By.XPATH, "//a[@ns-qa='QA Automation Engineer']")
+    QA_TXT_HR = (By.ID, "gnewtonJobDescriptionText")
+
+    def click_crs_btn(self):
+        """
+        click on careers button
+        """
+        self.click(*self.CRS_BTN)
+
+    def click_qa_eng_mia(self):
+        """
+        click on QA Automation Engineer in Miami button inside the iframe
+        """
+        self.driver.switch_to.frame(self.driver.find_element_by_id("gnewtonIframe"))
+        self.click(*self.QA_ENG_MIA)
+        sleep(4)
+
+    def qa_txt_hr(self, text):
+        """
+        verify page has a text 'As QA Automation Engineer you will develop automated test solutions'
+        """
+        self.verify_text(text, *self.QA_TXT_HR)
